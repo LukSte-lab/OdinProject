c2fad02b8ffa5882ccb9a7b462d094bd2f0fe50f
LukSte-lab <slobodan.eric@gmail.com>
2023-12-17 06:41:55 +0400

    new update

--- javascript/javascript-exercises-Odin/04_removeFromArray/removeFromArray.js
+++
@@ -1,7 +0,0 @@
-  var removeFromArray = function(...args) {
-    const array = args[0]
-    return array.filter(val => !args.includes(val))
-  }
-
-// Do not edit below this line
-module.exports = removeFromArray;
